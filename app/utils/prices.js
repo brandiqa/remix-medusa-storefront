b78e0c88c919632135c799fe04a03ce8e5558007
Michael Wanyoike <micwanyoike@gmail.com>
2022-04-04 11:51:10 +0300

    removed tax from formatPrice

--- app/utils/prices.js
+++ app/utils/prices.js
@@ -4,9 +4,6 @@ const locale = "en-US";
 // TODO: Allow users to select region currency (usd | eur)
 const regionCurrency = "usd";
 
-// TODO: Detect user currency/Allow currency selection (usd | eur)
-const taxRate = 0;
-
 export function formatPrice(variant) {
   const price = variant.prices.find(
     (price) => price.currency_code == regionCurrency
@@ -14,5 +11,5 @@ export function formatPrice(variant) {
   return new Intl.NumberFormat(locale, {
     style: "currency",
     currency: regionCurrency,
-  }).format((price.amount * (1 + taxRate)) / 100);
+  }).format(price.amount / 100);
 }
